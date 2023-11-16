5be7281342f3707838a6fd948e5a5ef3457b8023
Paul <paul.ivanov1990@gmail.com>
2023-11-17 00:45:38 +0300

    Some fix

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.PROD === 'production' ? process.env.JWT_SECRET : 'dev-secret';
 
 const corsOptions = {
-  origin: ['http://localhost:3001'],
+  origin: ['http://localhost:5173'],
   credentials: true,
 };
 
